177a0ad9ef83c875cbe986797d0cc09b9ff7f3bc
Piyush Kumar <955piyush@gmail.com>
2025-06-15 00:48:40 +0530

    frontend started

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './index.css'
+import Header from './components/Header'
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+    <div className="min-h-screen p-6 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900"> 
+      <Header />
+    </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+     <div className='text-center'>
+       <h1 className="text-3xl md:text-4xl font-bold text-white mb-2">
+  Domain Analysis Dashboard
+</h1>
+<p className="text-slate-400 text-sm md:text-base">
+  Know the moment your website is down, Before your users do.
+</p>
+
+    </div>
+  )
+}
+
+export default Header
